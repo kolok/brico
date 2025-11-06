5804b3ce8dda93b8bdc223f96f0b812dd08fd162
Nicolas Oudard <nicolas@oudard.org>
2025-11-06 07:43:28 +0100

    Mise à jour du style

--- webapp/tailwind.config.js
+++ webapp/tailwind.config.js
@@ -1,20 +1,20 @@
 /** @type {import('tailwindcss').Config | null} */
 
 module.exports = {
-  content: [
-    "templates/**/*.html",
-    "static/to_compile/**/*.{js,ts,svg}",
-  ],
+  content: ["templates/**/*.html", "static/to_compile/**/*.{js,ts,svg}"],
   corePlugins: {
     preflight: false,
   },
-  safelist: [
-  ],
+  safelist: [],
   theme: {
     colors: {
       info: "#000091",
       black: "black",
       white: "white",
+      primary: "#0066cc",
+      dark: "#3c3d40",
+      "gray-200": "#e5e7eb",
+      "blue-600": "#2563eb",
     },
   },
 }
