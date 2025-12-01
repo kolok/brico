af1eed9a99e93ae6b85e6d14f70db6745e6b0878
Nicolas Oudard <nicolas@oudard.org>
2025-12-02 00:36:27 +0100

    upgrade tailwind

--- webapp/audits/forms.py
+++ webapp/audits/forms.py
@@ -19,7 +19,7 @@ class CommentForm(forms.ModelForm):
                     "placeholder": "Add a comment...",
                     "class": (
                         "w-full px-3 py-2 border border-gray-300 rounded-md"
-                        " focus:outline-none focus:ring-2 focus:ring-blue-500"
+                        " focus:outline-hidden focus:ring-2 focus:ring-blue-500"
                     ),
                 }
             ),
